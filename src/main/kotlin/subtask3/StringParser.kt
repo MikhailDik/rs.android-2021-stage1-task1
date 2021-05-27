9d6ce451d072ea82efec6597c87bc9c57974e73c
MikhailDik <89502834657@mail.ru>
2021-05-27 14:59:57 +1000

    subtask 3 OK

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,9 +1,46 @@
 package subtask3
 
+import java.util.*
+
 class StringParser {
+
+    fun findExp(str: String, b: Char, e: Char): String {
+        val s: Stack<Int> = Stack()
+        for (i in str.indices) {
+            if (str[i] == b) s.push(i)
+            if ((str[i] == e) and !s.empty()) {
+                s.pop()
+                if (s.empty()) return str.substring(1, i)
+            }
+        }
+        return "0"
+    }
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+
+        val stringList: MutableList<String> = mutableListOf()
+
+        for (i in inputString.indices) {
+            if (inputString[i] == '<')  stringList.add(findExp(inputString.substring(i),'<','>'))
+            if (inputString[i] == '(')  stringList.add(findExp(inputString.substring(i),'(',')'))
+            if (inputString[i] == '[')  stringList.add(findExp(inputString.substring(i),'[',']'))
+        }
+
+        return stringList.toTypedArray()
+
     }
+
 }
+
+/*fun main() {
+
+    val parser = StringParser()
+
+    val str: String = "<A(B)>(<C[D])E>"
+    val tA = parser.getResult(str)
+
+    val a = 10
+
+
+}*/
